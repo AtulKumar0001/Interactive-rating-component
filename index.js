c5737f89c8ba3e08c09bad09df2a5552630e268a
Atul <95268815+AtulKumar0001@users.noreply.github.com>
2023-01-10 21:28:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-let change_rating = document.querySelector(".rated");
-let stars = document.querySelectorAll(".stars");
-// for (let i = 0; i < document.querySelectorAll(".stars").length; i++) {
-//     document.querySelectorAll(".stars")[i].addEventListener("click", function () {
-//       let new_rating = this.innerHTML
-//       console.log(new_rating);
-//       change_rating.innerHTML = new_rating;
-//     });
-// }
-    stars.forEach(star => {
-        star.addEventListener("click", function(){
-            console.log(star.innerHTML);
-            change_rating.innerHTML = star.innerHTML;  
-        })
-    });
-
-let hide_1 = document.querySelector(".card2");
-let hide_2 = document.querySelector(".card")
-let submit = document.querySelector(".submit");
-submit.addEventListener("click", function(){
-  console.log(this);
-  hide_2.classList.add("hidden");
-  hide_1.classList.remove("hidden");
-  
-})
+"use strict";
+const submit = document.querySelector(".submit");
+const change_rating = document.querySelector(".rated");
+let rated = false;
+let rating = 0;
+const hide_1 = document.querySelector(".card2");
+const hide_2 = document.querySelector(".card");
+const stars = document.querySelectorAll(".stars");
+for (let i = 0; i < stars.length; i++) {
+  stars[i].addEventListener("click", function () {
+    rating = Number(stars[i].innerHTML);
+    rated = true;
+  });
+}
+const submit2 = function () {
+  if (rated) {
+    hide_2.classList.add("hidden");
+    hide_1.classList.remove("hidden");
+    change_rating.innerHTML = rating;
+    rated = false;
+  } else alert("Please Rate first");
+};
+submit.addEventListener("click", submit2);
